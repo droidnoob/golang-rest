3d69736c9c9f2f64bef31d0abf4b250ae90d1ea8
Ananthakrishnan <kannan4mi3@gmail.com>
2019-10-30 15:14:12 +0530

    Initial Commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	fmt.Println("server running")
+	r.Run()
+}
